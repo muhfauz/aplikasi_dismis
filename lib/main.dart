a7f89d824968b2d340115e51e32259005d232b3e
Sutrisno <sutrisno.stz@bsi.ac.id>
2022-11-02 20:42:38 +0700

    pasang faker dan dismisable

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  Faker faker = Faker();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +26,18 @@ class MyHomePage extends StatelessWidget {
       body: ListView.builder(
         itemCount: 100,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(),
-            title: Text(
-              'Data 1',
-              style: TextStyle(fontSize: 20),
+          return Dismissible(
+            crossAxisEndOffset: 0.0,
+            key: Key(index.toString()),
+            child: ListTile(
+              leading: CircleAvatar(),
+              title: Text(
+                faker.internet.userName(),
+                style: TextStyle(fontSize: 20),
+              ),
+              subtitle: Text(faker.lorem.sentence()),
+              trailing: TextButton(onPressed: () {}, child: Text('Klik')),
             ),
-            subtitle: Text('Data 2'),
-            trailing: TextButton(onPressed: () {}, child: Text('Klik')),
           );
         },
       ),
