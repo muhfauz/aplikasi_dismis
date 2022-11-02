27d61986132a84d6b0f65774638f874577514153
Sutrisno <sutrisno.stz@bsi.ac.id>
2022-11-02 20:06:37 +0700

    pasang listbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data'),
+      ),
+      body: ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(),
+            title: Text(
+              'Data 1',
+              style: TextStyle(fontSize: 20),
+            ),
+            subtitle: Text('Data 2'),
+            trailing: TextButton(onPressed: () {}, child: Text('Klik')),
+          );
+        },
+      ),
+    );
+  }
 }
